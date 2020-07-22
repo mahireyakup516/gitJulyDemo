d4e415da929b631d75f2a8a59762f22c412c896b
mahireyakup516 <67755246+mahireyakup516@users.noreply.github.com>
2020-07-21 17:48:46 -0700

    Update HelloGit.java

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 
 public class HelloGit {
-
+		// added some comments on this file using GitHub edit 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("My second Print Statement");
